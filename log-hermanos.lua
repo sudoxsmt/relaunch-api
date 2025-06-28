82552b40583f55f19233011021f6c1c50b693963
sudoxsmt <3althaza11@gmail.com>
2025-06-29 03:12:20 +0700

    Create log-hermanos.lua

---
+++ log-hermanos.lua
@@ -0,0 +1,27 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+local gagId = 126884695634066
+if game.PlaceId == gagId then
+    task.wait(30) -- ระยะเวลารอ ถ้าเปิดหลายจอเเนะนำให้ตั้งเป็น 20 วิ
+    getgenv().Hermanos_Settings = {
+        ['key'] = var1,
+        ['PC'] = var2,
+        ['Seeds'] = {"Sunflower","Candy Blossom","Moon Blossom","Candy Blossom","Dragon Pepper","Elephant Ears","Honeysuckle"},
+        ['Pets'] = {"Queen Bee","Raccoon","Dragonfly","Disco Bee","Butterfly","Mimic Octopus","Fennec Fox","Toucan"},
+        ['Gears'] = {"Master Sprinkler"},
+        ['Eggs'] = {"Anti Bee Egg","Bee Egg","Night Egg","Mythical Egg","Bug Egg","Paradise Egg","Oasis Egg"}
+
+    }
+    task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/gag-main.lua'))() end)
+else 
+    getgenv().Hermanos_Settings = {
+        ['key'] = var1,
+        ['PC'] = var2,
+        ['Sword'] = {'Cursed Dual Katana', 'Shark Anchor'},
+        ['Gun'] = {''},
+        ['Accessories'] = {},
+        ['Fruit'] = {'Kitsune', 'Leopard', 'Dragon (West)', 'Gas', 'Yeti',  'Dough', 'Dragon (East)'},
+    }
+    task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/script.lua'))() end)
+end
