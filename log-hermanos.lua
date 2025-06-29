d2ef5f39c1e628e8f9eef3f10f2b2c59e9ca28d6
sudoxsmt <3althaza11@gmail.com>
2025-06-29 19:38:25 +0700

    Update log-hermanos.lua

--- log-hermanos.lua
+++ log-hermanos.lua
@@ -20,7 +20,7 @@ if game.PlaceId == gagId then
         ['key'] = var1,
         ['PC'] = var2,
         ['Seeds'] = {"Sunflower","Candy Blossom","Moon Blossom","Candy Blossom","Dragon Pepper","Elephant Ears","Honeysuckle"},
-        ['Pets'] = {"Queen Bee","Raccoon","Dragonfly","Disco Bee","Butterfly","Mimic Octopus","Fennec Fox","Toucan"},
+        ['Pets'] = {"Queen Bee","Raccoon","Dragonfly","Disco Bee","Butterfly","Mimic Octopus","Fennec Fox","Toucan","Hyacinth Macaw"},
         ['Gears'] = {"Master Sprinkler"},
         ['Eggs'] = {"Anti Bee Egg","Bee Egg","Night Egg","Mythical Egg","Bug Egg","Paradise Egg","Oasis Egg"}
 
