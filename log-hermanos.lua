1dd25b0f6ee41ab297694b9dc1139371b8ba9031
sudoxsmt <3althaza11@gmail.com>
2025-06-29 14:55:31 +0700

    Update log-hermanos.lua

--- log-hermanos.lua
+++ log-hermanos.lua
@@ -1,7 +1,19 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
+
+local function isAllowedPlace(placeId, allowedList)
+	for _, id in ipairs(allowedList) do
+		if placeId == id then
+			return true
+		end
+	end
+	return false
+end
+
 local gagId = 126884695634066
+local bfList = {2753915549, 4442272183, 7449423635}
+
 if game.PlaceId == gagId then
     task.wait(30) -- ระยะเวลารอ ถ้าเปิดหลายจอเเนะนำให้ตั้งเป็น 20 วิ
     getgenv().Hermanos_Settings = {
@@ -14,7 +26,7 @@ if game.PlaceId == gagId then
 
     }
     task.spawn(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/hermanos-dev/hermanos-script/main/gag-main.lua'))() end)
-else 
+elseif isAllowedPlace(game.PlaceId, bfList) then
     getgenv().Hermanos_Settings = {
         ['key'] = var1,
         ['PC'] = var2,
