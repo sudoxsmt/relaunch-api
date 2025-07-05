eca1e8358daae3634e106966ca5842729639399b
sudoxsmt <3althaza11@gmail.com>
2025-07-06 00:53:41 +0700

    Update vc-config.lua

--- vc-config.lua
+++ vc-config.lua
@@ -1,9 +1,16 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
+repeat
+    task.wait()
+until game:IsLoaded()
+repeat
+    task.wait()
+until game.Players
+repeat
+    task.wait()
+until game.Players.LocalPlayer
 task.wait(20) -- ระยะเวลารอ ถ้าเปิดหลายจอเเนะนำให้ตั้งเป็น 20 วิ
 getgenv().Configuration = {
-    SEEDS = { -- กำหนด Seed ที่ต้องการให้ Farm
+    SEEDS = {
+        -- กำหนด Seed ที่ต้องการให้ Farm
         "Carrot",
         "Bamboo",
         "Mushroom",
@@ -11,9 +18,10 @@ getgenv().Configuration = {
         "Beanstalk",
         "Grape",
         "Sugar Apple",
-	"Burning Bud",
+        "Burning Bud",
     },
-    BuySeed = { -- กำหนด Seed ที่ต้องการซื้อจาก Seed Shop
+    BuySeed = {
+        -- กำหนด Seed ที่ต้องการซื้อจาก Seed Shop
         "Carrot",
         "Bamboo",
         "Mushroom",
@@ -21,9 +29,10 @@ getgenv().Configuration = {
         "Beanstalk",
         "Grape",
         "Sugar Apple",
-	"Burning Bud",
+        "Burning Bud",
     },
-    EGG = { -- กำหนด Egg ที่ต้องการให้ Farm
+    EGG = {
+        -- กำหนด Egg ที่ต้องการให้ Farm
         "Legendary Egg",
         "Mythical Egg",
         "Bug Egg",
@@ -33,10 +42,11 @@ getgenv().Configuration = {
         "Paradise Egg",
         "Oasis Egg",
         "Rare Summer Egg",
-	"Uncommon Egg",
-	"Dinosaur Egg",
+        "Uncommon Egg",
+        "Dinosaur Egg",
     },
-    BUYEGG = { -- กำหนด Egg ที่ต้องการซื้อจาก Egg Shop
+    BUYEGG = {
+        -- กำหนด Egg ที่ต้องการซื้อจาก Egg Shop
         "Bug Egg",
         "Night Egg",
         "Bee Egg",
@@ -44,23 +54,25 @@ getgenv().Configuration = {
         "Paradise Egg",
         "Oasis Egg",
         "Rare Summer Egg",
-	"Uncommon Egg",
+        "Uncommon Egg",
     },
-    PET_NAME = { -- กำหนกดชื่อ pet ที่ต้องการนำใส่ฟาร๋ม
+    PET_NAME = {
+        -- กำหนกดชื่อ pet ที่ต้องการนำใส่ฟาร๋ม
         "Chicken",
         "Rooster",
         "Kiwi",
         "Blood Kiwi",
-	"Capybara",
-	"Toucan",
-	"Ostrich",
+        "Capybara",
+        "Toucan",
+        "Ostrich",
     },
-    PETTOUPGRADE = { -- กำหนดชื่อ Pet ที่ต้องการนำไปใช้ในการ Upgrade
+    PETTOUPGRADE = {
+        -- กำหนดชื่อ Pet ที่ต้องการนำไปใช้ในการ Upgrade
         "Rooster",
         "Chicken",
         "Kiwi",
-	"Ostrich",
-	"Blood Kiwi",
+        "Ostrich",
+        "Blood Kiwi"
     },
     SELL_PET = {
         -- กำหนดชื่อ pet ที่ต้องการขาย
@@ -105,22 +117,28 @@ getgenv().Configuration = {
         "Sea Turtle",
         "Orangutan",
         "Seal",
-	"Moon Cat",
-	"Hedgehog",
-	"Mole",
-	"Frog",
-	"Echo Frog",
-	"Night Owl",
-	"Petal Bee",
-	"Bear Bee",
-	"Toucan",
-	"Hyacinth Macaw",
-	"Pterodactyl",
-	"Triceratops",
-	"Raptor",
-	"Stegosaurus",
+        "Moon Cat",
+        "Hedgehog",
+        "Mole",
+        "Frog",
+        "Echo Frog",
+        "Night Owl",
+        "Petal Bee",
+        "Bear Bee",
+        "Toucan",
+        "Hyacinth Macaw",
+        "Pterodactyl",
+        "Triceratops",
+        "Raptor",
+        "Stegosaurus",
     },
-    PetDino = { -- กำหนดชื่อ Pet ที่ต้องการใช้ในการ Evo
+    BUY_GEAR = {
+        -- กำหนด Item ที่ต้องการซื้อจาก Gear Shop
+        "Godly Sprinkler",
+        "Master Sprinkler"
+    },
+    PetDino = {
+        -- กำหนดชื่อ Pet ที่ต้องการใช้ในการ Evo
         "Chicken",
         "Rooster",
         "Kiwi",
@@ -162,64 +180,62 @@ getgenv().Configuration = {
         "Sea Turtle",
         "Orangutan",
         "Seal",
-	"Moon Cat",
-	"Hedgehog",
-	"Mole",
-	"Frog",
-	"Echo Frog",
-	"Night Owl",
-	"Petal Bee",
-	"Bear Bee",
-	"Toucan",
-	"Hyacinth Macaw",
-	"Pterodactyl",
-	"Triceratops",
-	"Raptor",
-	"Stegosaurus",
+        "Moon Cat",
+        "Hedgehog",
+        "Mole",
+        "Frog",
+        "Echo Frog",
+        "Night Owl",
+        "Petal Bee",
+        "Bear Bee",
+        "Toucan",
+        "Hyacinth Macaw",
     },
-    BUY_GEAR = { -- กำหนด Item ที่ต้องการซื้อจาก Gear Shop
-        "Godly Sprinkler",
-        "Master Sprinkler",
+    Craft_Event = {
+        -- กำหนดชื่อที่ต้องการจะคราฟ และจำนวน
+        {Name = "Honeysuckle", Amount = 5},
+        {Name = "Ancient Seed Pack", Amount = 20},
+        {Name = "Mutation Spray Amber", Amount = 5}
     },
-    Craft_Event = { -- กำหนดชื่อที่ต้องการจะคราฟ และจำนวน
-	{ Name = "Honeysuckle", Amount = 5 },
-	{ Name = "Ancient Seed Pack", Amount = 20 },
-        { Name = "Mutation Spray Amber", Amount = 5 },
+    EVENTSHOP = {
+        -- กำหนดชื่อ Event ที่ต้องการทำ
+        {Name = "Summer Seed Pack", Amount = 2},
+        {Name = "Crafters Seed Pack", Amount = 5},
+        {Name = "Oasis Egg", Amount = 2}
     },
-    EVENTSHOP = { -- กำหนดชื่อ Event ที่ต้องการทำ
-        { Name = "Summer Seed Pack", Amount = 2 },
-        { Name = "Crafters Seed Pack", Amount = 5 },
-        { Name = "Oasis Egg", Amount = 2 },
-    },
-
     SELL_PET_MIN_COUNT = 1, -- จำนวน Pet ที่จะขายเมื่อมีมากกว่าหรือเท่ากับจำนวนนี้
     SELL_PET_KEEP_COUNT = 1, -- จำนวน Pet ที่จะเก็บไว้เมื่อขาย Pet
     SELL_PET_KEEP_WEIGHT = 8, -- จำนวน Pet ที่จะเก็บไว้เมื่อขาย Pet ตามน้ำหนัก
-
     OpenEgg = true, -- true/false เปิด Auto Open Egg
     PlaceEgg = true, -- true/false เปิด Auto Place Egg
     BuyEgg = true, -- true/false เปิด Auto Buy Egg
-
     UpgradePetSlot = true, -- true/false เปิด Auto Upgrade Pet Slot
     UpgradeEggSlot = true, -- true/false เปิด Auto Upgrade Egg Slot
     UpgradePetInventory = false, -- true/false เปิด Auto Upgrade Pet Inventory
-
     AUTOFEEDPETS = true, -- true/false เปิด Auto Feed Pet
     AutoEvent = true, -- true/false เปิด ส่ง Event
     AutoFavEvent = true, -- true/false เปิด Auto Fav Event
     OPEN_SEEDPACK = true, -- true/false เปิด Night Seed Pack
-
     MoneyEnough = 5000000, -- ต้องมีเงินมากกว่าเท่าไหร่ถึงจะซื้อ Egg and Item
     Fpsboost = true, -- true/false เปิด Fpsboost
     MaxCropInFarm = 250,
     OpenFlowerSeedPack = 1,
     CollectBatch = 10, -- จำนวนการเก็บของในฟาร์มต่อครั้ง
-    WEBHOOK_URL = getgenv().Discord , -- Webhook ที่จะส่งไปที่ Discord
-    NOTIFY_PETS = { "Dragonfly", "Raccoon", "Queen Bee","Disco Bee","Butterfly","Mimic Octopus","Fennec Fox","T-Rex"},
-
-    COLLECT_MINUTES = { -- กำหนดเวลาในการเก็บของในฟาร์ม ตามจำนวนเงินที่มี
-        { Money = 1000000, Minutes = 2 },
-        { Money = 0, Minutes = 0 },
+    WEBHOOK_URL = getgenv().Discord, -- Webhook ที่จะส่งไปที่ Discord
+    NOTIFY_PETS = {
+        "Dragonfly",
+        "Raccoon",
+        "Queen Bee",
+        "Disco Bee",
+        "Butterfly",
+        "Mimic Octopus",
+        "Fennec Fox",
+        "T-Rex"
     },
+    COLLECT_MINUTES = {
+        -- กำหนดเวลาในการเก็บของในฟาร์ม ตามจำนวนเงินที่มี
+        {Money = 1000000, Minutes = 2},
+        {Money = 0, Minutes = 0}
+    }
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a30dea03b26012f29a97cd81cda4083a.lua"))()
