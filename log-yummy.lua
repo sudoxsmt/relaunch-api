72e94baa93f727690bb18499b05583cb5bb3effe
sudoxsmt <3althaza11@gmail.com>
2025-07-13 12:10:10 +0700

    Update log-yummy.lua

--- log-yummy.lua
+++ log-yummy.lua
@@ -14,7 +14,6 @@ local gagId = 126884695634066
 local bfList = {2753915549, 4442272183, 7449423635}
 
 if game.PlaceId == gagId then
-  task.wait(30)
   _G.Config = { 
 		UserID = getgenv().UserID , 
 		discord_id = getgenv().DiscordId , 
