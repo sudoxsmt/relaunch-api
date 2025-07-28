b4ffbc74cdad74b03eb62ed12498d706ab609995
sudoxsmt <3althaza11@gmail.com>
2025-07-28 19:11:33 +0700

    Update aya-gag.lua

--- aya-gag.lua
+++ aya-gag.lua
@@ -1,6 +1,3 @@
-repeat task.wait() until game:IsLoaded()
-repeat task.wait() until game.Players
-repeat task.wait() until game.Players.LocalPlayer
 getgenv().ConfigsKaitun = {
 	["Block Pet Gift"] = true,
 
