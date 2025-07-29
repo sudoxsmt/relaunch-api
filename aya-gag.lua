57a9b9929012ca2ca04a42f977560b4bbac203cb
sudoxsmt <3althaza11@gmail.com>
2025-07-29 23:25:20 +0700

    Update aya-gag.lua

--- aya-gag.lua
+++ aya-gag.lua
@@ -163,7 +163,7 @@ getgenv().ConfigsKaitun = {
 			},
 			["Limit Upgrade"] = 2,
 			["Equip When Done"] = {
-				["Kodama"] = { 1, 70 ,1},
+				["Kodama"] = { 2, 70 ,1},
 				["Corrupted Kodama"] = { 1, 70 ,1},
 				["Koi"] = { 1, 70 ,1},
 				["Ostrich"] = { 1, 70 ,4}, -- 1 mean equip only 1 pet , 70 mean equip only level pet lower than 70 the one on the last mean first priority will ues first if possible 
