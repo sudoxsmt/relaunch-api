65ea13c71c0350868e6547a8c0a5201b8700e648
sudoxsmt <3althaza11@gmail.com>
2025-08-24 09:11:03 +0700

    Update aya-gag.lua

--- aya-gag.lua
+++ aya-gag.lua
@@ -248,7 +248,8 @@ getgenv().ConfigsKaitun = {
 				["Hotdog Daschund"] = { 1, 60 ,1},
 				["Sunny-Side Chicken"] = { 1, 75 ,1 , true},
 				["Seal"] = { 1, 75 ,1 , true},
-				["Rooster"] = { 1, 60 ,1 ,true},
+				["Rooster"] = { 1, 60 ,1 ,true},,
+				["Lemon Lion"] = { 1, 60 ,1 ,true},
 			},
 			["Limit Upgrade"] = 5,
 			["Equip When Done"] = {
@@ -263,6 +264,7 @@ getgenv().ConfigsKaitun = {
 				["Sunny-Side Chicken"] = { 1, 100 ,1},
 				["Seal"] = { 1, 100 ,1},
 				["Rooster"] = { 1, 60 ,1},
+				["Lemon Lion"] = { 1, 100 ,1},
 			},
 		},
 		Locked_Pet_Age = 100, -- pet that age > 60 will lock
