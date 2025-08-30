41b12feea4dc2f10676dc51fb589a08929a98a50
sudoxsmt <3althaza11@gmail.com>
2025-08-30 23:48:07 +0700

    Update aya-gag.lua

--- aya-gag.lua
+++ aya-gag.lua
@@ -158,6 +158,26 @@ getgenv().ConfigsKaitun = {
 		Dino = true,
 		["Fairy Event"] = {
 			Minimum_Money = 10_000_000, -- minimum money to start play this event
+			Rewards_Item = {
+				"FairyPoints",
+				"Enchanted Egg",
+				"Enchanted Seed Pack",
+			},
+			Upgrade = {
+				Mode = "Order", -- Order (mean will up on order), Chepest, Highest
+				Order = { -- top upgrade first, not put mean not upgrade
+					"Fairy Spawn Amount",
+					"Loose Fairy Spawn Amount",
+					"Glimmer Multiplier",
+					"Fairy Event Duration",
+				},
+				Limit = {
+					["Glimmer Multiplier"] = 5, -- max 10
+					["Loose Fairy Spawn Amount"] = 4, -- max 4
+					["Fairy Event Duration"] = 5, -- max 10
+					["Fairy Spawn Amount"] = 9, -- max 9
+				}
+			}
 		},
 		MaxMoney_Restocks = 100_000_000,
 		Restocks_When_Come = { -- will restock when Sprout Egg come on shop
