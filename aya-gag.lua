e225b80af2cbf6cd2c587a436e21b749c068544c
sudoxsmt <3althaza11@gmail.com>
2025-09-07 14:37:05 +0700

    change config

--- aya-gag.lua
+++ aya-gag.lua
@@ -143,7 +143,7 @@ getgenv().ConfigsKaitun = {
 
 	Events = {
 		["Fairy Event"] = {
-			Minimum_Money = 10_000_000, -- minimum money to start play this event
+			Minimum_Money = 5_000_000, -- minimum money to start play this event
 			Rewards_Item = {
 				"Pet Shard Glimmering",
 				"Enchanted Egg",
@@ -163,7 +163,7 @@ getgenv().ConfigsKaitun = {
 				Limit = {
 					["Glimmer Multiplier"] = 5, -- max 10
 					["Loose Fairy Spawn Amount"] = 10, -- max 10
-					["Fairy Event Duration"] = 5, -- max 10
+					["Fairy Event Duration"] = 10, -- max 10
 					["Fairy Spawn Amount"] = 9, -- max 9
 				}
 			}
