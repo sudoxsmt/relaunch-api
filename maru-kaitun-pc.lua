a8e3616dc134135971e68a6ee035a4bd9b0f1fdd
sudoxsmt <3althaza11@gmail.com>
2025-10-02 14:53:18 +0700

    change config

--- maru-kaitun-pc.lua
+++ maru-kaitun-pc.lua
@@ -1,7 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
-repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
 task.wait(30)
 _G.Team = "Pirate" -- Marine / Pirate
 _G.MainSettings = {
