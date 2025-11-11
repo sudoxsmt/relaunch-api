f607ab756e7f44f167813f6febeb789a3194d9cd
sudoxsmt <3althaza11@gmail.com>
2025-11-11 07:20:41 +0700

    add new maru config

---
+++ new-maru.lua
@@ -0,0 +1,43 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+_G.OneClickSettings = {
+    ["LockFruits"] = {
+        "Dough-Dough",
+        "Dragon-Dragon",
+        "Kitsune-Kitsune",
+        "Mammoth-Mammoth",
+        "Leopard-Leopard",
+        "T-Rex-T-Rex",
+        "Buddha-Buddha",
+        "Yeti-Yeti",
+        ["Gas-Gas",
+        "Gravity-Gravity",
+        ["Creation-Creation",
+        "Phoenix-Phoenix",
+        "Spirit-Spirit",
+        "Shadow-Shadow",
+        "Venom-Venom",
+        "Control-Control",
+        "Tiger-Tiger"
+    },
+    ["MainFruits"] = {
+        "Dark-Dark", "Magma-Magma"
+    },
+    ["RegularFruits"] = {
+        "Flame-Flame",
+        "Ice-Ice",
+        "Quake-Quake",
+        "Light-Light",
+        "Dark-Dark",
+        "Spider-Spider",
+        "Rumble-Rumble",
+        "Magma-Magma"
+    },
+    ["FragmentsCap"] = 10000,
+    ["Farm Shark Anchor"] = true
+}
+getgenv().key = getgenv().MaruPcKey
+getgenv().id = getgenv().MaruPcId
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruComkak/main/PCBit.lua"))()
