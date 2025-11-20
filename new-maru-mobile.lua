ef0854f4a65636ccc79f997b80226af9455cc8b0
sudoxsmt <3althaza11@gmail.com>
2025-11-20 17:41:00 +0700

    added new maru

---
+++ new-maru-mobile.lua
@@ -0,0 +1,44 @@
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players
+repeat task.wait() until game.Players.LocalPlayer
+repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+_G.Team = "Pirate"
+_G.OneClickSettings = {
+    ["MainIsFarmLevel"] = true, 
+    ["LockFruits"] = {
+        "Dough-Dough",
+        "Dragon-Dragon",
+        "Kitsune-Kitsune",
+        "Mammoth-Mammoth",
+        "Leopard-Leopard",
+        "T-Rex-T-Rex",
+        "Buddha-Buddha",
+        "Yeti-Yeti",
+        "Gas-Gas",
+        "Gravity-Gravity",
+        "Creation-Creation",
+        "Phoenix-Phoenix",
+        "Spirit-Spirit",
+        "Shadow-Shadow",
+        "Venom-Venom",
+        "Control-Control",
+        "Tiger-Tiger"
+    },
+    ["MainFruits"] = {
+        "Dark-Dark", "Magma-Magma"
+    },
+    ["RegularFruits"] = {
+        "Flame-Flame",
+        "Ice-Ice",
+        "Quake-Quake",
+        "Light-Light",
+        "Dark-Dark",
+        "Spider-Spider",
+        "Rumble-Rumble",
+        "Magma-Magma"
+    },
+    ["FragmentsCap"] = 10000,
+    ["Farm Shark Anchor"] = true
+}
+getgenv().Script_Mode = "Kaitun_Script"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
