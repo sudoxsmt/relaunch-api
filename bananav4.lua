4149fd35e5004b6010e63308c01f894bf7011c44
sudoxsmt <3althaza11@gmail.com>
2025-11-28 02:50:36 +0700

    test v4

--- bananav4.lua
+++ bananav4.lua
@@ -1,7 +1,25 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().ConfigV4 = {
     ["Account Up Gear"] = {
-      ""
+        "Hilarypzhtw30610,
+        "Robinafvivl51421",
+        "Elliemkojn18748",
+        "Tovadprse58612",
+        "Athenaknjge01895",
+        "Chandatlrgd30459",
+        "Rinapzpfj47849",
+        "Dustyyjzcc29268",
+        "Callaorgwl53467",
+        "Moragrcsm40488",
+        "Veradalvp40731",
+        "Tessipjveg48560",
+        "Wynneislht17287"
+        "Neilacsske94825",
+        "Pauletrlfl12427",
+        "Pansyildzm99491",
+        "Marislsyck01475",
+        "Leighdavph11730",
+        "Heddieatois98523",
     },
     ["Account Help"] = {
       "adasiatrade",
