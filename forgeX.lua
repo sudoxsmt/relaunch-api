cfb83d42a3464d0bc31589f0dc29dfa6276d9258
sudoxsmt <3althaza11@gmail.com>
2025-12-08 20:45:38 +0700

    add new kaitun

---
+++ forgeX.lua
@@ -0,0 +1,42 @@
+getgenv().Configuration = { 
+     Enabled = true,
+     Pickaxe = {
+        ['Arcane Pickaxe'] = true, -- // ห้ามปิดบังคับต้องมี
+        ['Demonic Pickaxe'] = true, -- // เปิด / ปิด ได้ไม่บังคับ 
+        ['Magma Pickaxe'] = true, -- // เปิด / ปิด ได้ไม่บังคับ
+     },
+     Weapon = {
+        ['Eye Tachi'] = true, -- // ห้ามปิดบังคับต้องมี
+     },
+     Armor = {
+        ['Lightite Knight Chestplate'] = true,
+        ['Lightite Knight Leggings'] = true,
+        ['Lightite Knight Helmet'] = true,
+     },
+     Performance = {
+        ['FPS Lock'] = {
+            ['Enabled'] = true,
+            ['FPS'] = 15,
+        },
+     },
+     Races = {
+        ['Auto Roll'] = false,
+        ['Races'] = {
+            'Angel',
+            'Demon',
+        },
+     },
+     Codes = {
+        'PEAK!',
+        'SORRYFORSHUTDOWN',
+        '300K!',
+        '400K!',
+        '100KLIKES',
+        '200K!',
+     },
+     Misc = {
+        ['Tween Speed'] = 50,
+     },
+};
+_G.Authorize = getgenv().keyX
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0fbaa2aa75e1ef5d4da3dd2e849494c8.lua")){'Exclusive'}
