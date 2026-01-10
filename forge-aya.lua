6f87db3ae9097be6f529d414bdde95044e4b33d2
sudoxsmt <3althaza11@gmail.com>
2026-01-11 02:16:15 +0700

    added f aya

---
+++ forge-aya.lua
@@ -0,0 +1,37 @@
+getgenv().ConfigsKaitun = {
+	World3Farm = true,
+	BypassTp = true,
+
+	Race = { "Demon", "Angel", "Archangel" }, -- reroll
+	Pickaxe = {
+		["Magma"] = true,
+		["Demonic"] = true,
+		["Void"] = false,
+		-- The Arcane, Prismatic is warranty auto doing
+	},
+	SellPickaxe = { -- sell when didnt use
+		["Stone Pickaxe"] = true,
+		["Bronze Pickaxe"] = true,
+		["Iron Pickaxe"] = true,
+		["Titanium Pickaxe"] = true,
+		["Aqua Pickaxe"] = true,
+	},
+	End_game_craft = {
+		Weapon = {
+
+		},
+		Armor = {
+			["Helmet"] = {
+
+			},
+			["Chestplate"] = {
+				
+			},
+			["Leggings"] = {
+				
+			}
+		}
+	}
+}
+License = getgenv().KeyAYA
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Real-Aya/Loader/main/Init.lua'))()
