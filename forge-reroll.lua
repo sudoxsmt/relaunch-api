04a40475b1b566fe774fb591f697d26800281a5d
sudoxsmt <3althaza11@gmail.com>
2026-01-11 01:47:42 +0700

    Update forge-reroll.lua

--- forge-reroll.lua
+++ forge-reroll.lua
@@ -1,27 +1,20 @@
-_G.targetName = { "Archangel" } -- Developer, Orc, Demon, Vampire, Undead, Felynx, Zombie, Archangel, Fairy, Minotaur, Dragonborn, Dwarf, Shadow, Angel, Elf, Golem, Human, Orc Lord, Goblin
-_G.codes = { "FORGWEEKEND!" }
+getgenv().Configs = {
+    ["Race Priority"] ={
+        "Archangel",
+        "Angel",
+        "Demon"
+    },
+    ["Codes"] = {
+        "FORGWEEKEND!"
+    },
+    ["Settings"] = {
+        ["Horsty"] = {
+            ["Enabled"] = false -- เปิดถ้าต้องการใช้คู้กับม้านะงับ ( ก็ควรใช้แหละ )
+        },
+        ["FpsCap"] = 15
+    },
+    ["BUY_SELIWARE_"] = "https://kawafuji.rexzy.xyz/"
+}
+-- Made By Masterp & Ai
 
-repeat task.wait(.1) until game:IsLoaded()
-local Players = game:GetService("Players")
-local plr = Players.LocalPlayer
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Shared = ReplicatedStorage:WaitForChild("Shared")
-local Reroll = Shared.Packages.Knit.Services.RaceService.RF.Reroll
-local RedeemCode = Shared.Packages.Knit.Services.CodeService.RF.RedeemCode
-
-local Knit = require(Shared.Packages.Knit)
-local PlayerController = Knit.GetController("PlayerController")
-local Replica = PlayerController.Replica
-
-for _, code in pairs(_G.codes) do
-    RedeemCode:InvokeServer(code)
-end
-
-while Replica.Data.Spins > 0 and task.wait(.1) do
-    if table.find(_G.targetName, Replica.Data.Race) then break end
-    Reroll:InvokeServer()
-end
-
-local messages = "Auto Reroll Complete: ".. Replica.Data.Race .. " Reroll Left: ".. Replica.Data.Spins
-_G.Horst_SetDescription(messages)
-_G.Horst_AccountChangeDone()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Achitsak/PRRoblox/main/masterp/the_forge_reroll.lua"))()
